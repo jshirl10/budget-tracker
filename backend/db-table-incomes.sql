b69d45177d69c55fe760b5b92ba4594bf0b9e175
Josh Shirley <46604173+jshirl10@users.noreply.github.com>
2020-10-26 20:10:12 -0600

    Update db-table-incomes.sql

--- backend/db-table-incomes.sql
+++ backend/db-table-incomes.sql
@@ -9,4 +9,27 @@
 INSERT INTO incomes (user_id, amount, income_date, summary)
 VALUES (1, 375.69, '12/31/2020', 'Last paycheck of the year');
 
--- TODO the rest of the routes
+/*
+To get all incomes of a user
+for GET - get incomes
+*/
+SELECT *
+FROM incomes
+WHERE user_id = 1;
+
+/*
+To modify a previously inserted income
+for PUT - edit incomes
+*/
+UPDATE incomes
+SET amount = '123456789',
+    income_date = '12/31/2020',
+    description = 'Last Paycheck';
+
+/*
+To delete an income entry
+for DELETE - delete income
+*/
+DELETE
+FROM incomes
+WHERE income_id = 1;
